cbcc25276e35947e3d0fc27ce5d3c34b94e99b15
MaryamHasnain11 <mhasnain.bese15seecs@seecs.edu.pk>
2018-02-28 01:54:25 -0800

    1

---
+++ Forms/OnlineForm.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="OnlineForm.aspx.cs" Inherits="Forms.OnlineForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        	First Name<br />
+			<asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+			<br />
+			<br />
+			Last Name<br />
+			<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+			<br />
+			<br />
+			Email<br />
+			<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+			<br />
+			<br />
+			Country<br />
+			<br />
+			<asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+			<br />
+			<br />
+			
+			Gender<br />
+&nbsp;<asp:RadioButton ID="rb1" Text="Male" runat="server" />
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<asp:RadioButton ID="rb2" Text="Female" runat="server" />
+		
+        	<br />
+		
+        	<br />
+			<asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+		
+        </div>
+    </form>
+</body>
+</html>
---
+++ Forms/OnlineForm.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Forms
+{
+	public partial class OnlineForm : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void TextBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Button1_Click(object sender, EventArgs e)
+		{
+			String fName = TextBox1.Text.ToString();
+			String lName = TextBox2.Text.ToString();
+			String email = TextBox3.Text.ToString();
+			String country = TextBox4.Text.ToString();
+
+			OleDbConnection conn = new OleDbConnection();
+			//Use a string variable to hold the ConnectionString.
+			conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;"
+			+ "Data Source=C:\\Users\\Intag\\Documents\\GitHub\\cms2\\Forms\\App_Data\\Database2.accdb";
+
+
+
+
+
+			//new
+			String query = @"INSERT INTO Users (FirstName,LastName,Username,Pasword) VALUES(@fName,@lname,@userName,@pasword)";
+			System.Data.OleDb.OleDbCommand cmd = new System.Data.OleDb.OleDbCommand(query, conn);
+			cmd.Parameters.AddWithValue("@fName", fName);
+			cmd.Parameters.AddWithValue("@lName", lName);
+			cmd.Parameters.AddWithValue("@userName", userName);
+			cmd.Parameters.AddWithValue("@pasword", pasword);
+
+			conn.Open();
+
+
+
+
+			cmd.ExecuteNonQuery();
+			Response.Write("<script>alert('Registration successful');</script>");
+		}
+	}
+}
